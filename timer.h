8f1fdb3a272d15921c6d91915b1bcb00c89c7059
John Rigby <john.rigby@vivint.com>
2023-08-25 15:07:03 -0600

    system and real time but not very useful

--- timer.h
+++ timer.h
@@ -4,33 +4,38 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/times.h>
+#include <sys/time.h>
 
 static struct tms start_cpu_time;
 static struct tms end_cpu_time;
-static clock_t start_real_time;
-static clock_t end_real_time;
+static struct timeval start_real_time;
+static struct timeval end_real_time;
 
 static inline void start_timer() {
-    start_real_time = times(&start_cpu_time);
+    gettimeofday(&start_real_time, NULL);
+    times(&start_cpu_time);
 }
 
 static inline void stop_timer() {
-    end_real_time = times(&end_cpu_time);
+    gettimeofday(&end_real_time, NULL);
+    times(&end_cpu_time);
 }
 
 static inline void print_statistics(int iterations) {
     long ticks_per_second = sysconf(_SC_CLK_TCK);
     double user_time = (double)(end_cpu_time.tms_utime - start_cpu_time.tms_utime) / ticks_per_second;
     double system_time = (double)(end_cpu_time.tms_stime - start_cpu_time.tms_stime) / ticks_per_second;
-    double real_time = (double)(end_real_time - start_real_time) / ticks_per_second;
-    double total_cpu_time = user_time + system_time;
-    double iterations_per_second = iterations / total_cpu_time;
+
+    double elapsed_real_time = (end_real_time.tv_sec - start_real_time.tv_sec) + 
+                               (end_real_time.tv_usec - start_real_time.tv_usec) / 1000000.0;
 
     printf("User time: %.2f seconds\n", user_time);
     printf("System time: %.2f seconds\n", system_time);
-    printf("Total CPU time: %.2f seconds\n", total_cpu_time);
-    printf("Real time: %.2f seconds\n", real_time);
-    printf("Iterations per second (based on CPU time): %.2f\n", iterations_per_second);
+    printf("Real time: %.2f seconds\n", elapsed_real_time);
+
+    double iterations_per_second = iterations / elapsed_real_time;
+
+    printf("Iterations per second: %.2f\n", iterations_per_second);
 }
 
 #endif // TIMER_H
